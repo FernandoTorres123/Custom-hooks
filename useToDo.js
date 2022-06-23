5afba6aaf08a3c0543d1585627a0983015f63c08
FernandoTorres123 <lfernandotorres23@gmail.com>
2022-06-23 12:18:38 -0500

    primer commit

---
+++ useToDo.js
@@ -0,0 +1,53 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState = [];
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('toDos')) || [];
+}
+
+export const useToDo = () => {
+
+    const [toDos, dispatchToDo] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+    }, [toDos]);
+
+    const handleNewToDo = (todo) => {
+        const action = {
+            type: '[TODO] Add ToDo',
+            payload: todo,
+        }
+
+        dispatchToDo(action);
+    }
+
+    const handleDeleteToDo = (id) => {
+        dispatchToDo({
+            type: '[TODO] Remove ToDo',
+            payload: id
+        });
+    }
+
+    const handleToggleToDo = (id) => {
+        // console.log({ id });
+        dispatchToDo({
+            type: '[TODO] Toggle ToDo',
+            payload: id
+        });
+    }
+
+    
+
+  return {
+    toDos,
+    handleNewToDo,
+    handleDeleteToDo,
+    handleToggleToDo,
+    toDosCount: toDos.length,
+    pendingToDosCount: toDos.filter( todo => !todo.done ).length,
+  }
+}
